63c27d322f040ae05ede4d77ed61316a6dab08d8
Phani <phani.ramavarapu@gmail.com>
2024-07-06 16:49:15 +0530

    sql interview questions easy 1

---
+++ LeetCode - Python/basics.py
@@ -0,0 +1,24 @@
+x = 5
+x + 3
+y = 34
+x + y
+
+19 + y
+
+
+
+name = 'youtube'
+name + 'rocks'
+
+a = 1
+print(a)
+print(type(a))
+
+b = "Harry"
+
+print(type(b))
+d = True
+e = None
+print(type(d))
+print(type(e))
+
